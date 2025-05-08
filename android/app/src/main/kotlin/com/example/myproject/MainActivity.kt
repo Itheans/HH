f9c601fc8808fdfe18c94be48e3dd0817d9b91d3
Itheans <eark2207@hotmail.com>
2025-05-08 09:06:09 +0700

    gradle บัค

--- android/app/src/main/kotlin/com/example/myproject/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myproject/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myproject
+package com.example.projectcs  // เปลี่ยนจาก com.example.myproject เป็น com.example.projectcs
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
